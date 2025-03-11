2ff8215247fbbc85f93078fc7237bcc4f7680d04
Cedrickpng <cedrick.burudi@student.moringaschool.com>
2025-03-11 21:12:49 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  return names.map(function(name) {
+  return `Thank you, ${name}, for the wonderful ${event} gift!`;
+  });
+  }
+  
+  function countDown(number) {
+  while (number >= 0) {
+  console.log(number);
+  number--;
+  }
+  }
+  
+  
